4b4d301f207e70520999a3e2ac652401c7ae83d8
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2025-01-27 11:59:20 -0500

    multi image tag build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+BASE_DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 CONTAINER_ENGINE=${CONTAINER_ENGINE:-docker}
 TAG=${1:-latest}
 OS=${OS:-"$(uname -s | tr '[:upper:]' '[:lower:]')"}
@@ -17,7 +18,13 @@ if [ -z "${ARCH}" ]; then
     esac
 fi
 
-${CONTAINER_ENGINE} build . --file Dockerfile \
+CONTAINER_FILE=${BASE_DIR}/build/latest/Containerfile
+if [ -d ${BASE_DIR}/build/${TAG} ]; then
+    CONTAINER_FILE=${BASE_DIR}/build/${TAG}/Containerfile
+fi
+
+${CONTAINER_ENGINE} build . --file ${CONTAINER_FILE} \
+    --no-cache \
     --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
     --platform ${OS}/${ARCH} \
     --tag michaelvaldron/hugo:${TAG}
