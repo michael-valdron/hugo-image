e13003d2e24061599520bb9d41c66b9835907d8a
Michael Valdron <michael.valdron@gmail.com>
2025-05-13 18:36:29 -0400

    Revert "change build structure"
    This reverts commit 414574e46daffad041b99544b218c5683e20ced8.

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 
 BASE_DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 CONTAINER_ENGINE=${CONTAINER_ENGINE:-docker}
-IMG_NAME=${1:-'localhost/hugo'}
-TAG=${2:-latest}
+TAG=${1:-latest}
 OS=${OS:-"$(uname -s | tr '[:upper:]' '[:lower:]')"}
 IMG_ARCH=${IMG_ARCH:-''}
 if [ -z "${IMG_ARCH}" ]; then
@@ -19,22 +18,13 @@ if [ -z "${IMG_ARCH}" ]; then
     esac
 fi
 
-if [[ "${TAG}" == "latest" ]]; then
-    TAG=$(cat ${BASE_DIR}/build/latest)
-fi
-
-if [[ "${TAG}" == "$(cat ${BASE_DIR}/build/latest)" ]]; then
-    LATEST_TAG_ARG="--tag ${IMG_NAME}:latest"
-fi
-
-CONTAINER_FILE=${BASE_DIR}/build/${TAG}/Containerfile
-if [ ! -d ${BASE_DIR}/build/${TAG} ]; then
-    echo "error: build file '${CONTAINER_FILE}' for '${TAG}' does not exist"
-    exit 1
+CONTAINER_FILE=${BASE_DIR}/build/latest/Containerfile
+if [ -d ${BASE_DIR}/build/${TAG} ]; then
+    CONTAINER_FILE=${BASE_DIR}/build/${TAG}/Containerfile
 fi
 
 ${CONTAINER_ENGINE} build . --file ${CONTAINER_FILE} \
     --no-cache \
     --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
     --platform ${OS}/${IMG_ARCH} \
-    --tag ${IMG_NAME}:${TAG} ${LATEST_TAG_ARG}
+    --tag michaelvaldron/hugo:${TAG}
